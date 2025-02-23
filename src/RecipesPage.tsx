771a48cd0f6ff4caa8b90f28222cba0b48e36dfc
Amy <leam7265@student.otago.ac.nz>
2025-02-23 15:48:34 +1300

    added recipespage

---
+++ src/RecipesPage.tsx
@@ -0,0 +1,12 @@
+import RecipeList from "./RecipeList";
+
+const RecipesPage = () => {
+  return (
+    <div>
+      <h1>Recipes</h1>
+      <RecipeList />
+    </div>
+  );
+};
+
+export default RecipesPage;
